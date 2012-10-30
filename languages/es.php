6b58c6b0f10111b2ac146d6608876b777980e708
Per Jensen <admin@perjensen-online.dk>
2012-10-30 18:39:11 +0100

    Spanish language file submitted by Leonardo

---
+++ languages/es.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Elgg mobilize spanish language
+ * @author Leonardo
+ *
+ * @package mobilize
+ * 
+ */
+
+$spanish = array(
+
+	'mobilize:info:teaser' => '¿Utilizar Descripcion de?',
+	'mobilize:label:color' => "Color Encabezado, ingrese codigo de color Hex - #4690D6",
+	'mobilize:label:fp' => "¿Desabilitar javascript de seleccion amigos?", 
+	'mobilize:label:logo' => "¿Desplegar tu logo?",
+	'mobilize:label:teaser' => 'Si seleccionas Campo Texto, ingresa la descripcion:',
+	'mobilize:menu' => "Sub menu",
+	'mobilize:navigation' => "Navegacion",
+	'mobilize:option:lang' => "Archivo de Idioma",
+	'mobilize:option:field' => "Campo Texto",
+	'mobilize:option:none' => "Nada",
+	'mobilize:pagetitle' => 'Este es Mobilize',
+	'mobilize:teaser' => 'Bienvenido a nuestro sitio movil.   Por favor ingresa o registrate.',
+
+);
+add_translation("es", $spanish);
+
