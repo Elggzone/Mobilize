5b511d4e4b3ae1b3a93d5ee5b0010374e4aa2c23
Per Jensen <admin@perjensen-online.dk>
2014-04-23 22:50:04 +0200

    Adds hungarian language file

---
+++ languages/hu.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ *
+ * Mobilize Hungarian language file
+ * @author Elggzone - perjensen-online.dk
+ *
+ * @package mobilize
+ * 
+ */
+ 
+$hungarian = array(
+
+	'admin:settings:mobilize'	=> 'Mobilize',
+	'mobilize:panel'			=> 'Bővítmény beállításai',
+
+	'mobilize:copyright' 	=> 'Copyright &copy; ',
+	'mobilize:elgg'      	=> 'Powered by Elgg',
+	'mobilize:info:general'	=> 'Általános:',
+	'mobilize:info:message'	=> 'Kezdőlap üdvözlő szöveg:',
+	'mobilize:info:teaser'	=> 'Szöveg használata innen:',
+	'mobilize:label:color' 	=> "Fejléc színe, üsd be a Hex színkódot - #4690D6",
+	'mobilize:label:fp'		=> "Ismerős választó javascript letiltása?",	
+	'mobilize:label:logo' 	=> "Jelenítsd meg a saját logódat",
+	'mobilize:label:teaser'	=> 'Írd be a szöveget:',
+	'mobilize:menu' 		=> "Almenü",
+	'mobilize:navigation' 	=> "Navigáció",
+	'mobilize:option:file'	=> "Nyelvi fájl",
+	'mobilize:option:field'	=> "Szövegmező",
+	'mobilize:option:none'	=> "Nincs",
+	'mobilize:pagetitle'	=> 'Ez a Mobilize',
+	'mobilize:teaser'		=> 'Köszöntünk a mobilbarát oldalunkon. Jelentkezz be vagy regisztrálj!',
+	'mobilize:tooltip' 		=> 'Elggzone meglátogatása',
+
+);			
+add_translation("hu", $hungarian);
+
+?>
